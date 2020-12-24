da678b7f8739d1c21fd9f2b1c8c2a95001540b90
Adam Marsh <me@adam2marsh.co.uk>
2020-12-24 22:26:04 +0000

    Started to work on delete

--- sync-photos.py
+++ sync-photos.py
@@ -8,8 +8,12 @@ from pathlib import Path
 
 outFolder = ".out/"
 iCloudFileName = "iCloudPhotoFileNames"
-
 momentsFileName = "momentsPhotoFileNames"
+resultsFileName = "results"
+
+excludeVideoFileExtensions = ['.MOV', '.mp4', '.MP4', '.AVI', '.mov', '.m4v']
+
+localConfigDict = {}
 
 def logIntoiCloud():
     global iCloudApi
@@ -41,7 +45,7 @@ def logIntoiCloud():
 def getFilenamesFromiCloud():
     listOfPhotos = []
 
-    for photo in iCloudApi.photos.albums['Script']:
+    for photo in iCloudApi.photos.all:
         print ("Adding " + photo.filename + " to list")
         listOfPhotos.append(photo.filename)
 
@@ -68,8 +72,31 @@ def getFilenamesFromMoments():
     with open(outFolder + momentsFileName + ".json", 'w', encoding='utf-8') as f:
         json.dump(momentsFileNames, f, ensure_ascii=False, indent=4)
 
-print("Starting sync process")
+def loadFileIntoConfigDict(fileName):
+    with open(outFolder + fileName + '.json') as json_file:
+        fileContents = json.load(json_file)
+        localConfigDict[fileName] = fileContents
 
+def compareFiles(one, two):
+    picturesToDelete = []
+    for photoName in localConfigDict[one]:
+        if not skipMovieFiles(excludeVideoFileExtensions, photoName):
+            if photoName not in localConfigDict[two]:
+                print (photoName + ' not exists ' + two + '; so could be one to delete')
+                picturesToDelete.append(photoName)
+    
+    with open(outFolder + "delete.json", 'w', encoding='utf-8') as f:
+        json.dump(picturesToDelete, f, ensure_ascii=False, indent=4)
+    print('I think there is ' + str(len(picturesToDelete)) + ' to delete')
+
+def skipMovieFiles(fileExtensions, name):
+    for ext in fileExtensions:
+        if ext in name:
+            return True
+    
+    return False
+
+print("Starting sync process")
 if checkForFile(outFolder + iCloudFileName):
     print("iCloud File list doesn't exist so getting")
     logIntoiCloud()
@@ -77,10 +104,15 @@ if checkForFile(outFolder + iCloudFileName):
     print("Downloading Filenames from iCloud")
     getFilenamesFromiCloud()
 else:
-    print("iCloud Filenames Fetched")
+    print("iCloud Filenames Already Fetched")
 
 if checkForFile(outFolder + momentsFileName):
     print("Moments File list doesn't exist so getting")
     getFilenamesFromMoments()
 else:
-    print("Moments Filenames Fetched")
+    print("Moments Filenames Already Fetched")
+
+loadFileIntoConfigDict(iCloudFileName)
+loadFileIntoConfigDict(momentsFileName)
+
+compareFiles(iCloudFileName, momentsFileName)
